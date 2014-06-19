1513db0edceef3c4f292974f01bd967f08046bb6
Michelle Phung <michelle.phung@wepay.com>
2014-06-19 10:41:56 -0700

    change name

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -1,2 +1,2 @@
-  <h1>Welcome to FreshCooks</h1>
+  <h1>Welcome to FreshCook</h1>
     <p>You can buy a lots of fresh and gourmet food sold here. If you are a chef and want to sell your food, please register.</p>
