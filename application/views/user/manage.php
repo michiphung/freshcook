67b3c7a032674cb42066f8e7db92765900e9b97f
Michelle Phung <michelle.phung@wepay.com>
2014-06-18 17:42:28 -0700

    Adjusted the appearance of the manage page

--- application/views/user/manage.php
+++ application/views/user/manage.php
@@ -1,15 +1,12 @@
 
 <html>
 <head>
+	<h1><font size="6"><center>Welcome to FreshCook! Please manage your payment account. </center></font></h1>
+	<br>
 	<style>
 	</style>
 </head>	
 <?php
-
-	$config = Kohana::$config->load('wepay');
-     //set API Version. Change this to the API Version you want to use.
-     $API_VERSION = "2014-01-08";
-     WePay::useStaging($config->get('client_id'), $config->get('client_secret'), $API_VERSION);
 
 	$user = Auth::instance()->get_user();
 	$chef = ORM::factory('chef')->where('email', '=', $user->email)->find();
@@ -21,11 +18,13 @@
 
         ?>
         <body>
+        	<center>
         	<div id="kyc_div"><div>
 				<script type="text/javascript" src="https://www.wepay.com/min/js/iframe.wepay.js">
 				</script>
 
-			    <iframe src="<?=$response->uri?>" width="800" height="550" border="0" frameborder="0"></iframe>
+			    <iframe src="<?=$response->uri?>" width="800" height="450" border="5" frameborder="1"></iframe>
+			</center>
 
 
 
