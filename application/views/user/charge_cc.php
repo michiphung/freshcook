345d0ee5a601557d12baad20f43a825f1d640ed5
Michelle Phung <michelle.phung@wepay.com>
2014-06-20 14:57:43 -0700

    change html syntax error

--- application/views/user/charge_cc.php
+++ application/views/user/charge_cc.php
@@ -1,4 +1,2 @@
 <h1>Sucessfully Purchased <? echo $food ?> from <? echo $kitchen ?> for <? echo $price ?>!</h1>
 <br />
-<p><a href="<? echo $return_uri ?>">← Back to <? echo $kitchen ?></a></p>
-<br />
