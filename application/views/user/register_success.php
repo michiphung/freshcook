bc0a29a80d8813f0550af51b5b3b898bdead36ae
Michelle Phung <michelle.phung@wepay.com>
2014-06-23 16:22:44 -0700

    fixing redirect links

--- application/views/user/register_success.php
+++ application/views/user/register_success.php
@@ -1,3 +1,3 @@
 <h1>Congratulations! You are now able to receive payments!</h1>
 <br>
-<a href="<? echo $return_uri ?>">Click here to view/edit your restaurant</a>
+<a href="<? echo $return_uri ?>">Back</a>
