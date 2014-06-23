d768223bc88927fc14615987d11189e8e354b86a
Michelle Phung <michelle.phung@wepay.com>
2014-06-23 15:27:14 -0700

    remove typo

--- application/classes/Controller/Wepayapi.php
+++ application/classes/Controller/Wepayapi.php
@@ -10,7 +10,6 @@ class Controller_Wepayapi extends Controller_Base {
             $API_VERSION = "2014-01-08";
             //WePay::useStaging('242', 'b5df4504c9', $API_VERSION);
             WePay::useStaging($config->get('client_id'), $config->get('client_secret'), $API_VERSION);
-            c
             // $redirect_uri = $base_url . 'wepayapi';
             // $scope = WePay::$all_scopes;
 
